20b43a26581a04a18f1b89d493307c607001b46c
Zokirov Xojiakbar <hojiakbar312@gmail.com>
2024-12-11 18:11:26 +0500

    SERVICE:WIP

--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -1,9 +1,11 @@
 import PlayMarket from "../assets/Icons/play_market.svg";
 import AppStore from "../assets/Icons/app_store.svg";
 import QRCode from "../assets/qr-code.svg";
+import Icon from "../assets/Icons/inspector1.svg";
 
 const Services = () => {
   return (
+    <div className="bg-[#f8f8fa] ">
     <div className="bg-[#f8f8fa] max-w-screen-xl mx-auto mb-12">
     <h2 className="text-3xl font-semibold text-center">Xizmatlar</h2>
     <div className="flex grid-cols-6 mt-6 gap-6 justify-start">
@@ -27,12 +29,50 @@ const Services = () => {
         </div>
     </div>
 
+    <div className="bg-gray-50 py-4">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {/* Card 1 */}
+          <div className="flex flex-row justify-center items-center bg-white p-8 rounded-2xl shadow hover:shadow-xl transition-shadow">
+            <img src={Icon} alt="Megaphone" className="w-6 h-6 mx-4 justify-center items-center"/>
+            <p className="text-center text-xl text-gray-800 font-medium">Ilmiy-o'quv amaliy markazi</p>
+          </div>
 
+          {/* Card 2 */}
+          <div className="flex flex-row justify-center items-center bg-white p-8 rounded-2xl shadow hover:shadow-xl transition-shadow">
+            <img src={Icon} alt="Megaphone" className="w-6 h-6 mx-4 justify-center items-center"/>
+            <p className="text-center text-xl text-gray-800 font-medium">Ilmiy-o'quv amaliy markazi</p>
+          </div>
 
+          {/* Card 3 */}
+          <div className="flex flex-row justify-center items-center bg-white p-8 rounded-2xl shadow hover:shadow-xl transition-shadow">
+            <img src={Icon} alt="Megaphone" className="w-6 h-6 mx-4 justify-center items-center"/>
+            <p className="text-center text-xl text-gray-800 font-medium">Ilmiy-o'quv amaliy markazi</p>
+          </div>
 
+          {/* Card 4 */}
+          <div className="flex flex-row justify-center items-center bg-white p-8 rounded-2xl shadow hover:shadow-xl transition-shadow">
+            <img src={Icon} alt="Megaphone" className="w-6 h-6 mx-4 justify-center items-center"/>
+            <p className="text-center text-xl text-gray-800 font-medium">Ilmiy-o'quv amaliy markazi</p>
+          </div>
+
+          {/* Card 5 */}
+          <div className="flex flex-row justify-center items-center bg-white p-8 rounded-2xl shadow hover:shadow-xl transition-shadow">
+            <img src={Icon} alt="Megaphone" className="w-6 h-6 mx-4 justify-center items-center"/>
+            <p className="text-center text-xl text-gray-800 font-medium">Ilmiy-o'quv amaliy markazi</p>
+          </div>
+
+          {/* Card 6 */}
+          <div className="flex flex-row justify-center items-center bg-white p-8 rounded-2xl shadow hover:shadow-xl transition-shadow">
+            <img src={Icon} alt="Megaphone" className="w-6 h-6 mx-4 justify-center items-center"/>
+            <p className="text-center text-xl text-gray-800 font-medium">Ilmiy-o'quv amaliy markazi</p>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div className="bg-[#f8f8fa] py-6 px-8 rounded-xl shadow-lg border border-gray-200 max-w-screen-xl mx-auto my-14">
-    <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center">
         <div className="max-w-lg">
             <h1 className="text-2xl font-semibold ">MyUzMobile - barchasi o'z qo'lingizda!</h1>
             <p>Mobil ilovamni yuklab olish uchun Google Play yoki App Store maketga quyidagi tugma orqali o'ting yoki QR kodni skaner qiling.</p>
@@ -48,15 +88,10 @@ const Services = () => {
         <div>
             <img src={QRCode} alt="QR Code" className="w-24 h-24" />
         </div>
+      </div>
+     </div>
     </div>
-    </div>
-
-
    </div>
-
-   
-
-
   );
 };
 
