5c3596107b2721190dd4cceb8160084674a73fdf
Zokirov Xojiakbar <hojiakbar312@gmail.com>
2024-12-19 01:27:36 +0500

    CODE RUB:Changed Some Styles

---
+++ src/Components/DropDown.tsx
@@ -0,0 +1,27 @@
+import { Dropdown, Menu, Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+
+const DropDown = () => {
+  const handleMenuClick = ({ key }: { key: string }) => {
+    console.log(`Selected language: ${key}`);
+    // You can add logic here to change the language
+  };
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="uz">Uzbek</Menu.Item>
+      <Menu.Item key="ru">Russian</Menu.Item>
+      <Menu.Item key="en">English</Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Dropdown overlay={menu} trigger={['click']}>
+      <Button>
+        Select Language <DownOutlined />
+      </Button>
+    </Dropdown>
+  );
+};
+
+export default DropDown;
